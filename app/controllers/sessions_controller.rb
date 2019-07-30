5fe7150944a64a343fa98cbb07a8d3f4aa8a4745
slumfrog <kishan.anil@gmail.com>
2019-07-30 10:50:38 +0100

    local updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,26 @@
 class SessionsController < ApplicationController 
 
-def new
-    
-end
+    def new
+        
+    end
 
-def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-        flash[:success] = "You're logged in"
-        redirect_to users_path(user)
-    else
-        flash.now[:danger] = "Something is wrong check your details"
-        render 'new'
-    end  
-end
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You're logged in"
+            redirect_to users_path(user)
+        else
+            flash.now[:danger] = "Something is wrong check your details"
+            render 'new'
+        end  
+    end
 
-def destroy
-    session[:user_id] = nil 
-    flash[:success] = "Logged out"
-    redirect_to root_path   
-end
-
-
+    def destroy
+        session[:user_id] = nil 
+        flash[:success] = "Logged out"
+        redirect_to root_path   
+    end
 
 
 end
